9173771d6e92a8e9357b417db6d3779db729e6e6
Cesar Tavares <cetava@gmail.com>
2020-11-12 20:24:06 -0300

    Upload inicial project 'DigitalPokedex'

---
+++ DigitlaPokedex/DigitlaPokedex/Classes/TabBarController.swift
@@ -0,0 +1,8 @@
+//
+//  TabBarController.swift
+//  DigitlaPokedex
+//
+//  Created by Cesar A. Tavares on 09/11/20.
+//
+
+import Foundation
---
+++ DigitlaPokedex/DigitlaPokedex/PokemonFavorites/PokemonFavoritesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PokemonFavoritesViewController.swift
+//  DigitlaPokedex
+//
+//  Created by Cesar A. Tavares on 09/11/20.
+//
+
+import UIKit
+
+class PokemonFavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
