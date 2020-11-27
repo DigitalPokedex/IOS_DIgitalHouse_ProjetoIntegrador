8fc651b7f46e08634d4dc7505615d9e11e333989
Cesar Tavares <cetava@gmail.com>
2020-11-27 12:12:52 -0300

    (refactor): Bordes Extensions

---
+++ DigitlaPokedex/Extensions/UIButtonBordersExtensions copy.swift
@@ -0,0 +1,42 @@
+//
+//  Borders.swift
+//  DigitlaPokedex
+//
+//  Created by Cesar A. Tavares on 20/11/20.
+//
+
+import Foundation
+import UIKit
+
+
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
