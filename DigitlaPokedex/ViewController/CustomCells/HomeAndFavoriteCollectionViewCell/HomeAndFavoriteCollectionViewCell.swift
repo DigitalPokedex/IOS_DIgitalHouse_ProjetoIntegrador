2e493cded7c15059b02fed6ba0265c6252d06c56
Jorge <jorgercj@dcomp.ufs.br>
2021-01-31 19:19:51 -0300

    (fix): fix pokemon name width in collection view

--- DigitlaPokedex/ViewController/CustomCells/HomeAndFavoriteCollectionViewCell/HomeAndFavoriteCollectionViewCell.swift
+++ DigitlaPokedex/ViewController/CustomCells/HomeAndFavoriteCollectionViewCell/HomeAndFavoriteCollectionViewCell.swift
@@ -62,12 +62,20 @@ class HomeAndFavoriteCollectionViewCell: UICollectionViewCell {
         }
     }
     
+    func setupPokemonName() {
+        let screenWidth = ScreenSettings.screenWidth
+        let cellWidth = ((screenWidth - 35) / 2)
+        pokemonName.translatesAutoresizingMaskIntoConstraints = false
+        pokemonName.widthAnchor.constraint(equalToConstant: cellWidth - 80).isActive = true
+    }
+    
     func setupFromWeb(_ pokemon: Pokemon) {
         let primaryType = pokemon.types[0].type.name.capitalizingFirstLetter()
         let url = URL(string: pokemon.sprites.other.officialartwork.frontDefault)
         
         pokemonImage.kf.setImage(with: url)
         pokemonName.text = pokemon.name.capitalizingFirstLetter()
+        pokemonName.text = "auhashuasuhusauhashuahuuasuhuhsauasuhasuhsuasauhshu"
         pokemonNumber.text = setId(id: pokemon.id)
         
         container.backgroundColor = getBackgroundColor(primaryType: primaryType)
@@ -82,7 +90,7 @@ class HomeAndFavoriteCollectionViewCell: UICollectionViewCell {
         let url = URL(string: pokemon.sprites!)
         
         let primaryType = pokemon.types![0].type.name.capitalizingFirstLetter()
-        container.backgroundColor = getBackgroundColor(primaryType: primaryType)
+        //container.backgroundColor = getBackgroundColor(primaryType: primaryType)
         
         container.layer.cornerRadius = 15
         
@@ -96,6 +104,7 @@ class HomeAndFavoriteCollectionViewCell: UICollectionViewCell {
     }
     
     func setup(pokemon: AnyObject?) {
+        setupPokemonName()
         if let setupData = pokemon as? CompletePokemonRealm  {
             setupFromDB(setupData)
         } else if let setupData = pokemon as? Pokemon {
